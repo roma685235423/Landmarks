bdcbce7953642f2f3a839826de658738a900be04
Roman Boiko <roma685235423@icloud.com>
2023-08-27 21:37:57 +0300

    Initial Commit

---
+++ SwiftUIExaple/SwiftUIExapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExapleApp.swift
+//  SwiftUIExaple
+//
+//  Created by Роман Бойко on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
